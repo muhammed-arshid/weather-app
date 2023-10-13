9aaf40f246edf0a206a14a42f632f9f2c04883c9
abhijith.s <abhijith.s@calpinetech.com>
2023-10-13 13:11:03 +0530

    recent search history added to ui

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Weather App
         </h1>
-        <div className='flex'>
+        <div className='flex max-h-[500px]'>
           <div className='relative flex flex-col justify-between min-w-[400px] h-[90vh] m-auto p-4 z-10'>
             <div className='relative justify-between pt-12'>
               <div className='flex flex-col items-left'>
@@ -79,14 +79,41 @@ export default function Home() {
             </div>
           </div>
         </div>
-        
+
+        <div className='bg-black/50 relative p-8 rounded-md h-auto'>
+          <p className='text-2xl text-center pb-6'>Recent searches in same city</p>
+          <div className='flex justify-between text-center h-auto'>
+            <div class="w-1/2 border-l-2 border-gray-400 h-auto"></div>
+            <div className='min-w-[300px]'>
+                <span className='text-xl'>Time </span><span className='font-bold text-2xl'>2023-10-13 09:23</span><br/>
+                <span className='text-xl'>Temp </span><span className='font-bold text-2xl'>91&#176;</span><br/>
+                <span className='text-xl'>Humidity </span><span className='font-bold text-2xl'>18%</span><br/>
+                <span className='text-xl'>Winds </span><span className='font-bold text-2xl'>90MPH-NE</span><br/>
+            </div>
+            <div class="w-1/2 border-l-2 border-gray-400 h-auto"></div>
+            <div className='min-w-[300px]'>
+                <span className='text-xl'>Time </span><span className='font-bold text-2xl'>2023-10-13 09:20</span><br/>
+                <span className='text-xl'>Temp </span><span className='font-bold text-2xl'>92&#176;</span><br/>
+                <span className='text-xl'>Humidity </span><span className='font-bold text-2xl'>15%</span><br/>
+                <span className='text-xl'>Winds </span><span className='font-bold text-2xl'>80MPH-NE</span><br/>
+            </div>
+            <div class="w-1/2 border-l-2 border-gray-400 h-auto"></div>
+            <div className='min-w-[300px]'>
+                <span className='text-xl'>Time </span><span className='font-bold text-2xl'>2023-10-13 09:15</span><br/>
+                <span className='text-xl'>Temp </span><span className='font-bold text-2xl'>93&#176;</span><br/>
+                <span className='text-xl'>Humidity </span><span className='font-bold text-2xl'>16%</span><br/>
+                <span className='text-xl'>Winds </span><span className='font-bold text-2xl'>96MPH-NE</span><br/>
+            </div>
+            <div class="w-1/2 border-l-2 border-gray-400 h-auto"></div>
+          </div>
+        </div>
 
       </main>
 
 
       <style jsx>{`
         main {
-          padding: 5rem 0;
+          padding: 1rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
@@ -108,7 +135,7 @@ export default function Home() {
             Courier New,
             monospace;
         }    
-      `}</style>
+      `}</style> 
 
       <style jsx global>{`
         html,
@@ -116,7 +143,6 @@ export default function Home() {
           background-image: url('/images/weather_bg.jpg');
           background-size: cover;
           background-position: center;
-          background-repeat: no-repeat;
           padding: 0;
           margin: 0;
           font-family:
